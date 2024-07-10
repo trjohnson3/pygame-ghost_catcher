900bc4f31c6a99651811c726cc4b056a9a0606ae
Tom Johnson <trjohnson3@gmail.com>
2024-07-10 09:48:26 -0500

    add class blueprints - game, player, monster

--- game.py
+++ game.py
@@ -13,6 +13,72 @@ pygame.display.set_caption("Ghost Catcher")
 FPS = 60
 clock = pygame.time.Clock()
 
+#Define classes
+class Game():
+    """A class to control game play"""
+    def __init__(self):
+        """Initialize the game object"""
+        pass
+
+    def update(self):
+        """Update game object"""
+        pass
+
+    def draw(self):
+        """Draw HUD and other to display"""
+        pass
+
+    def check_collisions(self):
+        """check for collisions"""
+        pass
+
+    def start_new_round(self):
+        """Populate screen with new monsters"""
+        pass
+
+    def choose_target(self):
+        """Choose target for player"""
+        pass
+
+    def pause(self):
+        """Pause the game"""
+        pass
+
+    def restart_game(self):
+        """Restart game is player chooses to continue"""
+        pass
+    
+
+class Player(pygame.sprite.Sprite):
+    """A player class that the user controls"""
+    def __init__(self):
+        """Initialize the player object"""
+        pass
+
+    def update(self):
+        """Update the player"""
+        pass
+
+    def warp(self):
+        """Allows player to warp to safe zone (bottom)"""
+        pass
+
+    def reset(self):
+        "Resets player position to bottom of the screen"
+        pass
+
+
+class Monster(pygame.sprite.Sprite):
+    """A class to create enemy monster objects"""
+    def __init__(self):
+        """initialize the monster"""
+        pass
+
+    def update(self):
+        """Update the monster"""
+        pass
+    
+
 #Main game loop
 running = True
 while running:
